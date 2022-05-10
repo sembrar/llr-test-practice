faccceb792ea60a0a79a5ac2677c016dc8c75b8c
sembrar <sagarpj95@gmail.com>
2022-05-10 13:23:56 +0530

    Changed subject indices in ids and string names to 0,1,2
    Reason: May use arrays for subject names
    
    On branch development
    Changes to be committed:
     modified:   app/src/main/java/com/ghsembrar/llrtestpreparation/MainActivity.java
     modified:   app/src/main/res/layout/activity_main.xml
     modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/ghsembrar/llrtestpreparation/MainActivity.java
+++ app/src/main/java/com/ghsembrar/llrtestpreparation/MainActivity.java
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String PACKAGE_NAME_FOR_KEY_PREFIX = "com.ghsembrar.llrtestpreparation.";
     public static final String INTENT_EXTRA_KEY_SUBJECT_INDEX = PACKAGE_NAME_FOR_KEY_PREFIX + "subject_index";
-
-    public static final int SUBJECT_INDEX_FIRST = 0;
-    public static final int SUBJECT_INDEX_SECOND = 1;
-    public static final int SUBJECT_INDEX_THIRD = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // AndroidStudio says onClick in xml is broken in older versions
-        findViewById(R.id.main_button_learn_subject1).setOnClickListener(v -> startLearnActivity(SUBJECT_INDEX_FIRST));
-        findViewById(R.id.main_button_learn_subject2).setOnClickListener(v -> startLearnActivity(SUBJECT_INDEX_SECOND));
-        findViewById(R.id.main_button_learn_subject3).setOnClickListener(v -> startLearnActivity(SUBJECT_INDEX_THIRD));
+        findViewById(R.id.main_button_learn_subject0).setOnClickListener(v -> startLearnActivity(0));
+        findViewById(R.id.main_button_learn_subject1).setOnClickListener(v -> startLearnActivity(1));
+        findViewById(R.id.main_button_learn_subject2).setOnClickListener(v -> startLearnActivity(2));
     }
 
     private void startLearnActivity(int subjectIndex) {
