ff600732783b57bcb658d83d5594749c10a5df72
sembrar <sagarpj95@gmail.com>
2022-06-04 15:37:44 +0530

    Disabled logs
    On branch main
    Changes to be committed:
     modified: app/src/main/java/com/ghsembrar/llrtestpreparation/CONSTANTS.java

--- app/src/main/java/com/ghsembrar/llrtestpreparation/CONSTANTS.java
+++ app/src/main/java/com/ghsembrar/llrtestpreparation/CONSTANTS.java
@@ -1,7 +1,7 @@
 package com.ghsembrar.llrtestpreparation;
 
 public class CONSTANTS {
-    public static final boolean ALLOW_DEBUG = true;
+    public static final boolean ALLOW_DEBUG = false;
     public static final String LOG_TAG_PREFIX = "DebugLogTag.";
 
     public static final String PACKAGE_NAME_FOR_PREFIX = "com.ghsembrar.llrtestpreparation.";
